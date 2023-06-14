4d105a50da3b5fcf927c5c656055a1f38f4aaafb
xiling <xiling0709@gmail.com>
2023-06-13 19:27:15 -0700

    change

--- frontend/src/pages/Post/Post.page.jsx
+++ frontend/src/pages/Post/Post.page.jsx
@@ -3,10 +3,16 @@ import axios from "axios";
 import { ArticleCardImage } from "../../components/misc/ArticleCardImage";
 import { SimpleGrid, Container } from "@mantine/core";
 import { useLoaderData } from "react-router-dom";
+import { Loader } from '@mantine/core';
+
 
 export const PostPage = () => {
   const posts = useLoaderData();
 
+  if (!posts) {
+    return <Loader />;
+  }
+  
   return (
     <Container>
       <SimpleGrid cols={3}>
